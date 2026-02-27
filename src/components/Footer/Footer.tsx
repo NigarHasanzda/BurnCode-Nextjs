360e4d519761d66543b941098c92b4617a76659f
NigarHasanzda <nigarhesenzade703@gmail.com>
2026-02-27 13:10:13 +0400

    Hero Section Lang+Responsiv+Animation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,39 +1,50 @@
 "use client";
 import React from 'react';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
+
+// Lokallaşdırma faylları
+import az from "@/locales/az.json";
+import en from "@/locales/en.json";
+import ru from "@/locales/ru.json";
 
 const Footer = () => {
+    const params = useParams();
+    const lang = (params?.lang as string) || "az";
+
+    const translations: any = { az, en, ru };
+    const t = translations[lang]?.footer || en.footer;
+
     return (
         <footer className="w-full bg-[#F7F8FD] pt-16 pb-10 px-4 md:px-20 lg:px-79 font-sans text-[#1a1a1a]">
             <div className="grid grid-cols-1 md:grid-cols-12 gap-10 mb-12">
                 <div className="md:col-span-6 lg:col-span-7">
                     <div className="flex items-center mb-6">
-                        <Link href="/" className="logo flex items-center text-[41px] " >
+                        <Link href={`/${lang}`} className="logo flex items-center text-[41px] " >
                             <h1 className="Burncode_Logo">burn<span>c</span>ode</h1>
                         </Link>
                     </div>
                     <p className="text-[#4b5563] text-sm leading-relaxed max-w-md mb-8">
-                        Burncode company has been serving its customers for more than 5 years.
-                        During this period, we have implemented a number of small and large projects.
-                        We offer you quality work and a reasonable price.
+                        {t.description}
                     </p>
                     <div className="space-y-3">
                         <p className="font-bold text-lg">Info@Burncode.Org</p>
                         <p className="font-bold text-lg">+994508683623</p>
                     </div>
                 </div>
+
                 <div className="md:col-span-3 lg:col-span-2">
-                    <h3 className="text-[22px] font-medium mb-6">Pages</h3>
+                    <h3 className="text-[22px] font-medium mb-6">{t.pagesTitle}</h3>
                     <ul className="space-y-4 text-[#9ca3af] text-m">
-                        <li><Link href="/" className="hover:text-black transition-colors">Home</Link></li>
-                        <li><Link href="/services" className="hover:text-black transition-colors">Services</Link></li>
-                        <li><Link href="/portfolio" className="hover:text-black transition-colors">Portfolio</Link></li>
-                        <li><Link href="/contact" className="hover:text-black transition-colors">Contact Us</Link></li>
+                        <li><Link href={`/${lang}`} className="hover:text-black transition-colors">{t.links.home}</Link></li>
+                        <li><Link href={`/${lang}/services`} className="hover:text-black transition-colors">{t.links.services}</Link></li>
+                        <li><Link href={`/${lang}/portfolio/1`} className="hover:text-black transition-colors">{t.links.portfolio}</Link></li>
+                        <li><Link href={`/${lang}/contact`} className="hover:text-black transition-colors">{t.links.contact}</Link></li>
                     </ul>
                 </div>
 
                 <div className="md:col-span-3 lg:col-span-3">
-                    <h3 className="text-[22px] font-medium mb-6">Socials</h3>
+                    <h3 className="text-[22px] font-medium mb-6">{t.socialsTitle}</h3>
                     <ul className="space-y-4 text-[#9ca3af] text-m">
                         <li><a href="#" className="hover:text-black transition-colors">Instagram</a></li>
                         <li><a href="#" className="hover:text-black transition-colors">Facebook</a></li>
@@ -42,18 +53,19 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
+
             <div className="border-t border-gray-200 pt-8 flex flex-col md:flex-row justify-between items-center text-sm text-[#4b5563]">
                 <p>
-                    Copyright © 2026 BurnCode. All rights reserved.
+                    {t.copyright}
                 </p>
                 <div className="flex gap-8 mt-4 md:mt-0 items-center">
-                    <Link href="/privacy" className="text-[#9ca3af] hover:text-black transition-colors">Privacy Policy</Link>
-                    <Link href="/terms" className="text-[#9ca3af] hover:text-black transition-colors">Terms Of Service</Link>
+                    <Link href={`/${lang}/privacy`} className="text-[#9ca3af] hover:text-black transition-colors">{t.privacy}</Link>
+                    <Link href={`/${lang}/terms`} className="text-[#9ca3af] hover:text-black transition-colors">{t.terms}</Link>
                     <button
                         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
                         className="font-semibold text-gray-700 hover:text-black"
                     >
-                        Go To Top
+                        {t.goToTop}
                     </button>
                 </div>
             </div>
--- src/components/_homesections/HeroSection/HeroSection.tsx
+++ src/components/_homesections/HeroSection/HeroSection.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import PrimaryButton from '@/components/Button/Button';
 import { useParams } from 'next/navigation';
-
+import WavyText from '../../../FadeInWhenVisible/WavyText';
+import FadeInRightWhenVisible from '../../../FadeInWhenVisible/FadeInWhenVisible';
+import FadeInFromBottom from '../../../FadeInWhenVisible/FadeFromBottom';
 
 export default function Hero() {
-const params = useParams();
-const lang = params?.lang as string || "az";
+  const params = useParams();
+  const lang = params?.lang as string || "az";
 
   // JSON faylları import edirik
   const locales: Record<string, any> = {
@@ -18,30 +20,36 @@ const lang = params?.lang as string || "az";
   const t = locales[lang]?.hero || locales["az"].hero; // fallback az
 
   return (
-    <section 
+    <section
       className="relative min-h-[80vh] bg-[#F7F8FD] w-full flex flex-col items-center justify-center py-20 px-4 overflow-hidden -mt-20 sm:mt-2 md:mt-0"
     >
-      <div className="absolute inset-0 z-0 bg-center bg-no-repeat bg-contain opacity-70 pointer-events-none"  style={{ backgroundImage: "url('/hero-bg.png')" }}/>
+      <div className="absolute inset-0 z-0 bg-center bg-no-repeat bg-contain opacity-70 pointer-events-none" style={{ backgroundImage: "url('/hero-bg.png')" }} />
 
       <div className="relative z-10 max-w-5xl mx-auto text-center">
-        <span className="text-[#5D5FEF] font-bold tracking-widest text-[12px] md:text-[16px] uppercase mb-1 block">
-          {t.subtitle}
+        <span className="text-[#5D5FEF] font-bold tracking-widest text-[12px] md:text-[16px] uppercase -mb-2 block">
+          <WavyText text={t.subtitle} />
         </span>
 
-        <h1 className="text-[32px] md:text-[90px] font-[650] text-[#1A1A1A] leading-[1.3] mb-2 tracking-tight">
-          {t.titleLine1} <br />
-          {t.titleLine2} <br />
-          <span className="text-[#5D5FEF]">{t.titleHighlight}</span>
-        </h1>
+        <FadeInRightWhenVisible>
+          <h1 className="text-[32px] md:text-[90px] font-[650] text-[#1A1A1A] leading-[1.3] mb-2 tracking-tight">
+            {t.titleLine1} <br />
+            {t.titleLine2} <br />
+            <span className="text-[#5D5FEF]">{t.titleHighlight}</span>
+          </h1>
+        </FadeInRightWhenVisible>
 
-        <p className="max-w-2xl mx-auto text-gray-400 text-[14px] md:text-lg font-normal leading-relaxed mb-12 px-4">
-          {t.description}
-        </p>
+        <FadeInFromBottom>
+          <p className="max-w-2xl mx-auto text-gray-400 text-[14px] md:text-lg font-normal leading-relaxed mb-6 px-4">
+            {t.description}
+          </p>
+        </FadeInFromBottom>
 
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-5">
-          <PrimaryButton text={t.button1} path="/consultation" />
-          <PrimaryButton text={t.button2} path="/portfolio/1" />
-        </div>
+        <FadeInFromBottom>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-5">
+            <PrimaryButton text={t.button1} path="/consultation" />
+            <PrimaryButton text={t.button2} path="/portfolio/1" />
+          </div>
+        </FadeInFromBottom>
       </div>
     </section>
   );
